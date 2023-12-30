96d1f2cddb1e9960190091a60c7f7903ed021d75
Aidan Mcphee <aidanmcpheecompsci@gmail.com>
2023-12-30 01:17:10 +0000

    fixed delayed pipes

--- Assets/Scripts/UpPipeMovement.cs
+++ Assets/Scripts/UpPipeMovement.cs
@@ -13,33 +13,33 @@ public class UpPipeMovement : MonoBehaviour
     public float deadzone = -45;
     public float heightOffset = 4;
     public float rnd;
-    public bool changeMovement = false;
-    private float timer = 0;
+    public bool changeMovement;
     public float startTime = 0;
     public bool hasMoved = false;
+    private readonly float delay = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        float lowestSpawnPoint = this.transform.position.y - heightOffset;
-        float highestSpawnPoint = this.transform.position.y + heightOffset;
+        changeMovement = false;
+        startTime = Time.time;
+        float lowestSpawnPoint = transform.position.y - heightOffset;
+        float highestSpawnPoint = transform.position.y + heightOffset;
         rnd = Random.Range(lowestSpawnPoint, highestSpawnPoint);
-        this.targetPosition = new Vector3(this.transform.position.x, rnd, 0);
-        startTime = Time.fixedTime;
+        targetPosition = new Vector3(  transform.position.x, rnd, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!changeMovement || hasMoved )
+        if ((!changeMovement) || hasMoved )
         {
+           
             MoveX();
-            timer += Time.fixedTime;
 
-            if (timer - startTime > 500)
+            if ((Time.time - startTime) > delay)
             {
                 changeMovement = true;
-                timer = 0;
             }
         }
         else
@@ -53,14 +53,7 @@ public class UpPipeMovement : MonoBehaviour
             }
         }
 
-
-
-        if (transform.position.y == this.targetPosition.y)
-        {
-            changeMovement = false; // Reset the flag to go back to moving left
-        }
-
-        if (this.transform.position.x < deadzone)
+        if (transform.position.x < deadzone)
         {
             Debug.Log("Pipe destroyed");
             Destroy(gameObject);
@@ -70,12 +63,12 @@ public class UpPipeMovement : MonoBehaviour
 
     float MoveX()
     {
-        this.transform.position = this.transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
-        return this.transform.position.x;
+        transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+        return transform.position.x;
     }
 
     void MoveY()
     {
-        this.transform.position = this.transform.position + (Vector3.up * moveSpeed) * Time.deltaTime;
+        transform.position = transform.position + (Vector3.up * moveSpeed) * Time.deltaTime;
     }
 }
